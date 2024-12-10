d7f17effa205637e3ec00f2d66eed920bd406f0c
Kabesa-abraham <yebulaabraham@gmail.com>
2024-12-10 14:17:21 +0100

    Création du composant header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react'
+import './header.css'
+import {MdClose, MdCoffee, MdMenu} from 'react-icons/md'
+
+const Header = () => {
+
+    const [open ,setOpen] = useState(false);
+    const handleClose = () =>{
+        setOpen((prev) => prev===true? prev=false : prev=true  )
+    }
+
+    const [scrolled,setScrolled] = useState(false); //pour changer de couleur lorsque on scroll
+    useEffect(()=>{
+      const myScroll = () =>{
+        if(window.scrollY > 200){
+          setScrolled(true)
+        }else{
+          setScrolled(false)
+        }}
+        window.addEventListener("scroll", myScroll)
+  
+        return () =>{
+          window.removeEventListener("scroll" , myScroll)
+        }
+      },[])
+
+  return (
+    <div className={`${scrolled&&'shadow-lg'} fixed bg-transparent z-50 w-full py-5`} >
+        <div className='max-w-7xl mx-auto flex justify-between items-center px-7' >
+            <a href="#home">
+                <div className='flex items-center gap-3' >
+                    <MdCoffee className='text-2xl text-[#a86132] ' />
+                    <h2 className='font-bold' >Cafe Shop.</h2>
+                </div>
+            </a>
+
+            <div className={`${!open&&'activer'} navbarResponsive flex md:flex-row md:static items-center gap-10 `} >
+                <ul className='flex flex-col md:flex-row items-center gap-9 font-semibold relative optionContainer' >
+                    <li onClick={()=>{handleClose()}}>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li onClick={handleClose}>
+                        <a href="#repas">Menu</a>
+                    </li>
+                    <li onClick={handleClose}>
+                        <a href="#about">A propo</a>
+                    </li>
+                    <li onClick={handleClose}>
+                        <a href="#premiumDish">Produits</a>
+                    </li>
+                    <li onClick={handleClose}>
+                        <a href="#customerReview">Critiques</a>
+                    </li>
+                </ul>  
+            </div>
+
+            <div className='flex gap-5' >
+                <button
+                    className='py-2 border border-[#a16237] text-[#a16237] px-5 rounded-full text-sm font-semibold hover:bg-[#df7e3e] hover:text-white duration-150'
+                    >Se connecter
+                </button>
+
+                {
+                    open&& open?(
+                        <MdClose className='buttonNav md:hidden' onClick={handleClose} />
+                    ):(
+                        <MdMenu className='buttonNav md:hidden' onClick={handleClose} />
+                    )
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
